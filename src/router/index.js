5deb3f7ccfe3ddac5c64add5878ba78219d4fcca
Jesulonimii <jesulonimii.will@gmail.com>
2022-10-03 08:46:28 +0100

    home-almost

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'CineFlix',
       component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/MovieDetails.vue')
     }
   ]
 })
